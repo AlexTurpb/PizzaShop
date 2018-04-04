32d9eab885f93c78120b47a3e64d28bdc01f9b1c
Alex_Turpb <turpb41@gmail.com>
2018-04-04 21:55:30 +0300

    migration hair-up

--- db/migrate/20180401204741_add_products.rb
+++ db/migrate/20180401204741_add_products.rb
@@ -1,15 +1,68 @@
 class AddProducts < ActiveRecord::Migration[5.1]
   def change
-  	Product.create :title => 'Hawaiian', :description => 'This is Hawaiian pizza',	:price => 350, :size => 30,	:is_spicy => false,	:is_veg => false, :is_best_offer => false, :image => '/images/hawaiian.jpg'
+  	Product.create ({
+  		:title => 'Hawaiian',
+  		:description => 'This is Hawaiian pizza',
+  		:price => 350,
+  		:size => 30,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => false,
+  		:image => '/images/hawaiian.jpg'
+  	})
 
-  	Product.create :title => 'Pepperoni', :description => 'Nice Pepperoni pizza', :price => 450, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :image => '/images/pepperoni.png'
+  	Product.create ({
+  		:title => 'Pepperoni',
+  		:description => 'Nice Pepperoni pizza',
+  		:price => 450,
+  		:size => 30,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => true,
+  		:image => '/images/pepperoni.png'
+  	})
 
-  	Product.create :title => 'Vegetarian', :description => 'Amazing Vegetarian pizza', :price => 400, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :image => '/images/veggie.jpg'
+  	Product.create ({
+  		:title => 'Vegetarian',
+  		:description => 'Amazing Vegetarian pizza',
+  		:price => 400, :size => 30,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => false,
+  		:image => '/images/veggie.jpg'
+  	})
 
-  	Product.create :title => 'Balado', :description => 'An Indonesian fusion cuisine', :price => 425, :size => 30, :is_spicy => true, :is_veg => false, :is_best_offer => false, :image => '/images/balado.jpg'
+  	Product.create ({
+  		:title => 'Balado',
+  		:description => 'An Indonesian fusion cuisine',
+  		:price => 425,
+  		:size => 30,
+  		:is_spicy => true,
+  		:is_veg => false,
+  		:is_best_offer => false,
+  		:image => '/images/balado.jpg'
+  	})
 
-  	Product.create :title => 'Margherita', :description => 'Authentic Neapolitan pizza', :price => 375, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :image => '/images/margherita.jpg'
+  	Product.create ({
+  		:title => 'Margherita',
+  		:description => 'Authentic Neapolitan pizza',
+  		:price => 375,
+  		:size => 30,
+  		:is_spicy => false,
+  		:is_veg => false,
+  		:is_best_offer => false,
+  		:image => '/images/margherita.jpg'
+  	})
 
-  	Product.create :title => 'Quattro', :description => 'Pizza quattro stagioni', :price => 450, :size => 30, :is_spicy => true, :is_veg => false, :is_best_offer => true, :image => '/images/quattro_stagioni.jpg'
+  	Product.create ({
+  		:title => 'Quattro',
+  		:description => 'Pizza quattro stagioni',
+  		:price => 450,
+  		:size => 30,
+  		:is_spicy => true,
+  		:is_veg => false,
+  		:is_best_offer => true,
+  		:image => '/images/quattro_stagioni.jpg'
+  	})
   end
 end
