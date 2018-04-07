eb1388485149f14b2f08d5e346aadf478ac38df4
Alex_Turpb <turpb41@gmail.com>
2018-04-08 01:11:19 +0300

    add cart_total alert

--- public/app.js
+++ public/app.js
@@ -13,6 +13,7 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+	window.alert('Items in your cart ' + cart_total());
 	// Object.keys(localStorage).forEach(key => sum+=((localStorage[key])*1));
  // 	window.alert(sum);
 }
