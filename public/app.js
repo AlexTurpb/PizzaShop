beba028b83e9fb154cea9cdf05d2858700eeafda
Alex_Turpb <turpb41@gmail.com>
2018-04-08 02:05:56 +0300

    update orders input field

--- public/app.js
+++ public/app.js
@@ -14,8 +14,8 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 	window.alert('Items in your cart ' + cart_total());
-	// Object.keys(localStorage).forEach(key => sum+=((localStorage[key])*1));
- // 	window.alert(sum);
+	update_orders_input();
+
 }
 
 function cart_total()
@@ -34,3 +34,29 @@ function cart_total()
 	}
 	return cnt;
 }
+
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ','; 
+		}
+	}
+	return orders;
+}
+
+// Object.keys(localStorage).forEach(key => sum+=((localStorage[key])*1));
+// 	window.alert(sum);
